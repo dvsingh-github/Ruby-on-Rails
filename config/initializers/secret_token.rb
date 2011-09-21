fbb552997bad1eb5fe6c33c75b7603733f8745f0
Kunwar Digvijay <ethanhunt26@hotmail.com>
2011-09-22 01:10:56 +0530

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningHeroku::Application.config.secret_token = 'f4547e7d37c747101697c00e11d054fbae01478a18a073909f03bb9837a2c0c8857d244983ce571e6136ff738e8c519e1d5afe766ad2b8aa2d6f5696fd83befc'
